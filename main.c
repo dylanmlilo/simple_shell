cdc9f549bf53a505b8889b89ae8e0ec1cbbc3aad
dylanmlilo <dylanmlilo12@gmail.com>
2023-04-27 15:12:01 -0700

    siple shell

--- main.c
+++ main.c
@@ -1,8 +1,155 @@
 #include "shell.h"
 
-int main(int ac, char **av, char **env)
+void sig_handler(int sig);
+int execute(char **args, char **front);
+
+/**
+ * main - function that runs a simple UNIX command interpreter
+ *
+ * @argc: number of arguments
+ * @argv: arguments vector
+ *
+ * Return: the return value of the last executed command
+ */
+
+int main(int argc, char *argv[])
 {
-	if (ac == 1)
-		prompt(av, env);
-	return (0);
+	int ret, rein;
+	int *exe_ret = &rein;
+	char *prompt = "#cisfun$ ", *new_line = "\n";
+
+	ret = 0;
+	name = argv[0];
+	aliases = NULL;
+	hist = 1;
+	signal(SIGINT, sig_handler);
+	*exe_ret = 0;
+	environ = _copyenv();
+
+	if (!environ)
+	{
+		exit(-100);
+	}
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+		{
+			ret = handle_args(exe_ret);
+		}
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 10);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+			{
+				write(STDOUT_FILENO, new_line, 10);
+			}
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
+}
+
+/**
+ * sig_handler - function that prints a new prompt upon a signal
+ * @sig: signal
+ */
+
+void sig_handler(int sig)
+{
+	char *new_prompt = "\n#cisfun$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 20);
+}
+
+/**
+ * execute - function that executes a command in a child process
+ *
+ * @args: array of arguments
+ * @front: double pointer to the beginning of arguments
+ *
+ * Return: A corresponding error code if an error occurs
+ *         else the exit value of the last executed command
+ */
+
+int execute(char **args, char **front)
+{
+	pid_t child_pid;
+	int flag, status, ret;
+	char *command = args[0];
+
+	flag = status = 0;
+
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_location(command);
+	}
+
+	if ((access(command, F_OK) == -1) || !command)
+	{
+		if (errno == EACCES)
+		{
+			ret = (create_error(args, 126));
+		}
+		else
+		{
+			ret = (create_error(args, 127));
+		}
+	}
+	else
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+			{
+				free(command);
+			}
+			perror("Error child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+			{
+				ret = (create_error(args, 126));
+			}
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+	{
+		free(command);
+	}
+	return (ret);
 }
--- shell.h
+++ shell.h
@@ -4,9 +4,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <strings.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
-void prompt(char **av, char **env);
+#define END_OF_FILE -2
+#define EXIT -3
+
+	void prompt(char **av, char **env);
+	int proc_file_commands(char *file_path, int *exe_ret);
+	extern char **environ;
+	char *name;
+	int hist;
+
+/**
+ * struct builtin_s - New struct type defining builtin commands
+ *
+ * @name: name of the builtin command
+ * @f: function pointer to the builtin command's function
+ */
+
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **argv, char **front);
+} builtin_t;
+
+/**
+ * struct list_s - New struct type defining a linked list
+ *
+ * @dir: directory path
+ * @next: pointer to another struct list_s
+ */
+
+typedef struct list_s
+{
+	char *dir;
+	struct list_s *next;
+} list_t;
+
+/**
+ * struct alias_s - New struct defining aliases
+ *
+ * @name: name of the alias
+ * @value: value of the alias
+ * @next: pointer to another struct alias_s
+ */
+
+typedef struct alias_s
+{
+	char *name;
+	char *value;
+	struct alias_s *next;
+} alias_t;
+
+	/* Main function helpers */
+	ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+	list_t *get_path_dir(char *path);
+	int execute(char **args, char **front);
+	void free_list(list_t *head);
+	char **_strtok(char *line, char *delim);
+	char *get_location(char *command);
+	char *_itoa(int num);
+
+	alias_t *aliases;
+
+	/* Input helpers */
+	int call_args(char **args, char **front, int *exe_ret);
+	int run_args(char **args, char **front, int *exe_ret);
+	int handle_args(int *exe_ret);
+	int check_args(char **args);
+	void handle_line(char **line, ssize_t read);
+	void variable_replacement(char **args, int *exe_ret);
+	void free_args(char **args, char **front);
+	char **replace_aliases(char **args);
+	char *get_args(char *line, int *exe_ret);
+
+	/* Builtins */
+	int (*get_builtin(char *command))(char **args, char **front);
+	int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+	int shellby_help(char **args, char __attribute__((__unused__)) **front);
+	int shellby_exit(char **args, char **front);
+	int shellby_env(char **args, char __attribute__((__unused__)) **front);
+	int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+	int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+	int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+	char **_copyenv(void);
+	char **_getenv(const char *var);
+	void free_env(void);
+
+	/* String functions */
+	int _strlen(const char *str);
+	char *_strchr(char *str, char c);
+	int _strspn(char *str, char *accept);
+	char *_strcpy(char *dest, const char *src);
+	char *_strcat(char *dest, const char *src);
+	char *_strncat(char *dest, const char *src, size_t n);
+	int _strcmp(char *str1, char *str2);
+	int _strncmp(const char *str1, const char *str2, size_t n);
+
+	/* Error handlers */
+	int create_error(char **args, int err);
+	char *error_env(char **args);
+	char *error_1(char **args);
+	char *error_2_exit(char **args);
+	char *error_2_cd(char **args);
+	char *error_2_syntax(char **args);
+	char *error_126(char **args);
+	char *error_127(char **args);
+
+	/* Linked_list helpers */
+	alias_t *add_alias_end(alias_t **head, char *name, char *value);
+	list_t *add_node_end(list_t **head, char *dir);
+	void free_alias_list(alias_t *head);
+	void free_list(list_t *head);
+	void help_all(void);
+	void help_alias(void);
+	void help_cd(void);
+	void help_exit(void);
+	void help_help(void);
+	void help_history(void);
+	void help_env(void);
+	void help_setenv(void);
+	void help_unsetenv(void);
 
 #endif
-
